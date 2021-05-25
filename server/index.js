bc9dddcada84f56d02a6c7b21550a5dd119ac9d8
Patryk Makowski <patryk.makowski23@gmail.com>
2021-05-25 11:24:54 +0200

    Add route and ctrl creators

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
   res.send("I'm alive");
 });
 
